d0a0930602df2b4620c5473c656cfa4010835d18
Sabir Aliyev (local test env) <sabir.aliyev.itdep@gmail.com>
2024-11-07 15:24:57 +0400

    27.2 Using attributes in controller methods

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
+    private array $messages = [
+        " Hello!", " Hi!", " Bye!"
+    ];
+    
+    #[Route(path:"", name:"app_index")]
     public function index() {
-        return new Response("HI!");
+        return new Response(implode(",", $this->messages));
+    }
+
+    #[Route("/messages/{id}", name:"app_show_one")]
+    public function showOne(int $id) {
+        return new Response(($this->messages[$id]));
     }
 }
