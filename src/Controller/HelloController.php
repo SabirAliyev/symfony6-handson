73fcb00ed8cee0feb247eddc33c58065c2c72f3c
Sabir Aliyev (local test env) <sabir.aliyev.itdep@gmail.com>
2024-11-07 14:46:50 +0400

    27. Using routes.yaml file to bind controller.

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController {
+    public function index() {
+        return new Response("HI!");
+    }
+}
