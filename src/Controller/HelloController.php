19597e0afcf3bca164674b77ce055a7eef7333f8
Sabir Aliyev (local test env) <sabir.aliyev.itdep@gmail.com>
2024-11-15 11:12:00 +0400

    32. Twig filters & functions

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -15,7 +15,8 @@ class HelloController extends AbstractController {
     public function index(int $limit) {
         return $this->render('hello/index.html.twig', 
         [
-            'messages' => array_slice($this->messages, 0, $limit)
+            'messages' => $this->messages,
+            'limit' => $limit
         ]);
     }
 
